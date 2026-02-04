42463b9f2303873e127954c40191c6831d95ee9a
ytyfuigoljfh <23367bs@hvhs.school.nz>
2026-02-05 11:00:04 +1300

    added things

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+
+
+document.getElementById("welcomeMessage").innerText = "Welcome to the JavaScript Refresher page!";
+
 function buttonThing(){
     alert("Congratulation!\nYou pressed the button...");
 };
+
+function getFormInput() {
+    if (document.getElementById('fUserText').checkValidity()) {
+
+    var usertext = document.getElementById("text").value;
+
+    document.getElementById("welcomeMessage").innerText = usertext;
+    }
+}
